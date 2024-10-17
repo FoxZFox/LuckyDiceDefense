7fb2c00d8bb303d6a61fa39fb9a061d6324f759e
Fox <fook1222@gmail.com>
2024-10-17 14:25:33 +0700

    Fix GameplayUI

--- Assets/Scripts/Common/SoundSystem/SoundManager.cs
+++ Assets/Scripts/Common/SoundSystem/SoundManager.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Sirenix.OdinInspector.Editor.Validation;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
